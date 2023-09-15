fa8cb219bc654d6c57dc1cd800163328eace4cc3
KhyleSewpersaud <khylesewpersaud@gmail.com>
2023-09-14 22:46:55 -0400

    Beginning to get API to work and scrape data

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const puppeteer = require("puppeteer");
 const mongoose = require("mongoose");
 const Search = require("./models/Search");
 require('dotenv').config();
+const request = require('request-promise');
+const fs = require('fs')
+
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +17,8 @@ mongoose.connect(
   process.env.MONGO_URL
 );
 
-let lastItem;
+let lastSearch;
+
 
 app.post("/searches", (req, res) => {
     const search = JSON.stringify(req.body)
@@ -27,18 +31,51 @@ app.post("/searches", (req, res) => {
 app.get("/searchesGet", async (req, res) => {
     await mongoose.connect(process.env.MONGO_URL);
     const searchesGet = await Search.find({});
+    lastSearch = (JSON.parse(searchesGet[searchesGet.length - 1].search).body).replace(/^"(.*)"$/, '$1')
+    console.log('https://www.metro.ca/en/online-grocery/search?filter=' + lastSearch)
+
+    requester(lastSearch);
+
     res.json(searchesGet)
 })
 
+function requester(lastSearch) {
+    request('http://api.scraperapi.com/?api_key='+process.env.API_KEY+'&url=https://www.metro.ca/en/online-grocery/search?filter='+lastSearch)
+    .then(response => {
+        console.log(response)
+        fs.appendFile('here.txt', response, (err) => {
+            if (err) {
+                console.log(error)
+            } else {
+                console.log('yay')
+            }
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
 
 
 
-// async function nofrills() {
-//     const browser = await puppeteer.launch();
+// async function nofrills(search1) {
+//     const browser = await puppeteer.launch({headless: false});
 //     const page = await browser.newPage();
-//     await page.goto('https://www.nofrills.ca/search?search-bar='+search)
+
+//     await page.goto('https://www.nofrills.ca/search?search-bar='+search1)
+
+//     setTimeout(() => {
+        
+//     }, 40000)
+//     await page.screenshot({path: 'screenshot.png'})
+//     await browser.close()
+
+//     // const site = document.querySelector('div[data-track-product-index="5"]')
+//     // const title = site.querySelector('span[class=product-name__item--name]').innerHTML
 // }
 
 app.listen(4040);
+
+
 
 
--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
   let lastSearch;
 
   async function query(e) {
-    axios.post('http://localhost:4040/searches', {
+    await axios.post('http://localhost:4040/searches', {
       body: JSON.stringify(search)
     }).then(response => {
       console.log(response)
@@ -21,22 +21,7 @@ function App() {
       lastSearch = JSON.parse(response.data[response.data.length-1].search).body
       console.log(lastSearch)
     })
-  }, [])
-
-  // useEffect(() => {
-  //   axios.post('http://localhost:4040/lastSearch', {
-  //     body: lastSearch
-  //   }).then(response => {
-  //     console.log(response)
-  //   })
-  // })
-
-  // const nofrills = async () => {
-  //   const browser = await puppeteer.launch();
-  //   const page = await browser.newPage();
-  //   await page.goto(url+lastSearch)
-  //   browser.close();
-  // }
+  })
 
   return (
     <div>
@@ -68,13 +53,7 @@ function App() {
         </div>
         <div className="loblaws store">
           <h4>Loblaws</h4>
-          <img src="https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/3:2/w_6948,h_4632,c_limit/RoastChicken_RECIPE_080420_37993.jpg" />
-          <p>Chicken</p>
-          <h5>$100</h5>
-        </div>
-        <div className="food-basics store">
-          <h4>Food Basics</h4>
-          <img src="https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/3:2/w_6948,h_4632,c_limit/RoastChicken_RECIPE_080420_37993.jpg" />
+          <img src="https://assets.shop.loblaws.ca/products/21340952/b1/en/front/21340952_front_a01.png" />
           <p>Chicken</p>
           <h5>$100</h5>
         </div>
